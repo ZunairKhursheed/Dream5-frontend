b4fc2b59b7ace95bc00034eb2df414d4fb39095a
zakirAli <johndoe@example.com>
2023-11-03 23:40:37 +0500

    all

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "@/styles/layout.css";
 import "@/styles/global.scss";
 
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+
     <html lang="en">
       <body className={inter.className}>
         {children}
--- src/client/components/layout/header.js
+++ src/client/components/layout/header.js
@@ -1,10 +1,40 @@
 import React from "react";
 import style from "@/styles/layout/header.module.scss";
+// import style from "@/styles/global/";
+
 
 export const Header = () => {
   return (
-    <div className={style.header}>
-      <div className={style.inner}> Header</div>
-    </div>
+
+    <header className={style.header}>
+      <div className={`${style.header_top} container d-flex justify-content-center align-items-center px-2`}>
+        <div className="site-logo-container p-1">
+          <img
+            className="site-logo"
+            src="./Assets/Images/dream-logo-blue.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={`${style.header_bottom} container p-0 d-flex`}>
+        <div className="col header-tab-new active d-flex justify-content-center align-items-center">
+          Cricket
+        </div>
+        <div className="col header-tab-new d-flex justify-content-center align-items-center">
+          Football
+        </div>
+        <div className="col header-tab-new d-flex justify-content-center align-items-center">
+          Kabaddi
+        </div>
+      </div>
+    </header>
+
+
+
+
+    
+    // <div className={style.header}>
+    //   <div className={style.inner}> Header</div>
+    // </div>
   );
 };
