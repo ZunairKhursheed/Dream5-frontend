121392bc04540ec265b3425f61abadc0801d2c2a
zunairkhursheed <zunairkhursheed97@gmail.com>
2023-11-04 15:01:41 +0500

    car_ignore

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,5 @@
-import { Inter } from "next/font/google";
 import "@/styles/layout.css";
 import "@/styles/global.scss";
-
-
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -13,82 +8,73 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
         <div class="container">
           <footer class="footer container p-0 d-flex flex-column">
-                <div class="footer-top">
-          <div class="row d-flex">
-            <div
-              class="footer-element col-3 d-flex flex-column align-items-center justify-content-center"
-            >
-              <a
-                href=""
-                class="d-flex flex-column align-items-center footer-link"
-              >
-                <img
-                  // src="./Assets/Icons/Home 1.png"
-                  class="footer-btn-icon"
-                  alt=""
-                />
-                <p class="footer-btn-text">Home</p>
-              </a>
-            </div>
-            <div
-              class="footer-element col-3 d-flex flex-column align-items-center justify-content-center"
-            >
-              <a
-                href=""
-                class="d-flex flex-column align-items-center footer-link"
-              >
-                <img
-                  src="./Assets/Icons/My Matches.png"
-                  class="footer-btn-icon-1"
-                  alt=""
-                />
-                <p class="footer-btn-text">My Matches</p>
-              </a>
-            </div>
-            <div
-              class="footer-element col-3 d-flex flex-column align-items-center justify-content-center"
-            >
-              <a
-                href=""
-                class="d-flex flex-column align-items-center footer-link"
-              >
-                <img
-                  src="./Assets/Icons/people.png"
-                  class="footer-btn-icon"
-                  alt=""
-                />
-                <p class="footer-btn-text">Refer & Earn</p>
-              </a>
-            </div>
-            <div
-              class="footer-element col-3 d-flex flex-column align-items-center justify-content-center"
-            >
-              <a
-                href=""
-                class="d-flex flex-column align-items-center footer-link"
-              >
-                <img
-                  src="./Assets/Icons/Profile.png"
-                  class="footer-btn-icon"
-                  alt=""
-                />
-                <p class="footer-btn-text">Profile</p>
-              </a>
-            </div>
-          </div>
-          <div class="footer-bottom">
-            <div class="row">
-              <div class="copy-right col-12"></div>
-            </div>
-          </div>
+            <div class="footer-top">
+              <div class="row d-flex">
+                <div class="footer-element col-3 d-flex flex-column align-items-center justify-content-center">
+                  <a
+                    href=""
+                    class="d-flex flex-column align-items-center footer-link"
+                  >
+                    <img
+                      // src="./Assets/Icons/Home 1.png"
+                      class="footer-btn-icon"
+                      alt=""
+                    />
+                    <p class="footer-btn-text">Home</p>
+                  </a>
                 </div>
-              </footer>
+                <div class="footer-element col-3 d-flex flex-column align-items-center justify-content-center">
+                  <a
+                    href=""
+                    class="d-flex flex-column align-items-center footer-link"
+                  >
+                    <img
+                      src="./Assets/Icons/My Matches.png"
+                      class="footer-btn-icon-1"
+                      alt=""
+                    />
+                    <p class="footer-btn-text">My Matches</p>
+                  </a>
+                </div>
+                <div class="footer-element col-3 d-flex flex-column align-items-center justify-content-center">
+                  <a
+                    href=""
+                    class="d-flex flex-column align-items-center footer-link"
+                  >
+                    <img
+                      src="./Assets/Icons/people.png"
+                      class="footer-btn-icon"
+                      alt=""
+                    />
+                    <p class="footer-btn-text">Refer & Earn</p>
+                  </a>
+                </div>
+                <div class="footer-element col-3 d-flex flex-column align-items-center justify-content-center">
+                  <a
+                    href=""
+                    class="d-flex flex-column align-items-center footer-link"
+                  >
+                    <img
+                      src="./Assets/Icons/Profile.png"
+                      class="footer-btn-icon"
+                      alt=""
+                    />
+                    <p class="footer-btn-text">Profile</p>
+                  </a>
+                </div>
+              </div>
+              <div class="footer-bottom">
+                <div class="row">
+                  <div class="copy-right col-12"></div>
+                </div>
+              </div>
+            </div>
+          </footer>
         </div>
       </body>
     </html>
